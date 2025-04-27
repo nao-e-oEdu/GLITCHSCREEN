d33535a5811372aa3d070e5620ef92f34f53b11d
HickSouldrow <henriquedasilvamacedo6@gmail.com>
2025-04-27 15:20:49 -0300

    new background gradient

--- vite-project/src/layouts/LayoutPadrao/LayoutPadrao.jsx
+++ vite-project/src/layouts/LayoutPadrao/LayoutPadrao.jsx
@@ -6,7 +6,7 @@ const LayoutPadrao = () => {
   const { nomeUsuario } = useAppContext();
 
   return (
-    <div className="flex flex-col min-h-screen bg-stone-950 text-white">
+    <div className="flex flex-col min-h-screen text-white bg-gradient-to-br from-lime-900 via-stone-900 to-stone-950 animate-fundo">
       <Cabecalho />
       <main className="flex-grow space-y-20">
         <Conteudo>
