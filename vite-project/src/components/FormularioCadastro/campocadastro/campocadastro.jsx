2f39898e78a9a620308653e4bb44d7e403296933
Eduardo Sousa Cavalcante <131792350+EduardoSousaCavalcante@users.noreply.github.com>
2025-04-30 15:03:05 -0300

    Update campocadastro.jsx
    Atualização pra ficar funcionando lindamente

--- vite-project/src/components/FormularioCadastro/campocadastro/campocadastro.jsx
+++ vite-project/src/components/FormularioCadastro/campocadastro/campocadastro.jsx
@@ -1,15 +1,16 @@
-const CampoCadastro = ({ label, type, name, value }) => {
-    return (
-      <div className="mb-4">
-        <label className="block text-lime-800 text-sm font-medium">{label}</label>
-        <input
-          type={type}
-          name={name}
-          value={value}
-          className="w-full p-2 border border-stone-600 rounded-md focus:outline-none focus:ring-3 focus:ring-lime-800 focus:border-lime-800"
-        />
-      </div>
-    );
-  };
-  
-  export { CampoCadastro };
+const CampoCadastro = ({ label, type, name, value, onChange }) => {
+  return (
+    <div className="mb-4">
+      <label className="block text-lime-600 text-sm font-medium">{label}</label>
+      <input
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange} // Adicionando o onChange para capturar as mudanças
+        className="w-full p-2 border border-lime-800 rounded-md focus:outline-none focus:ring-2 focus:ring-lime-800 focus:border-lime-800"
+      />
+    </div>
+  );
+};
+
+export { CampoCadastro };
