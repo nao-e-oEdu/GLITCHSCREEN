3f0a8d68c17f38d4bb4fb22de1cd060159ee0fa1
LuanLancioni <luanlancioni12@gmail.com>
2025-04-30 11:45:28 -0300

    fix forms

--- vite-project/src/components/FormulariosGeral/FormularioLogin/FormularioLogin.jsx
+++ vite-project/src/components/FormulariosGeral/FormularioLogin/FormularioLogin.jsx
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 const FormularioLogin = () => {
   return (
     <div className="max-w-sm mx-auto bg-gradient-to-b from-stone-800 to-stone-700 p-8 rounded-xl shadow-lg border-2 border-lime-800 
-      relative before:absolute before:inset-0 before:border before:border-lime-800 before:rounded-xl before:shadow-[0_0_15px_#84cc16] before:animate-pulse">
-      <h2 className="text-2xl font-bold text-lime-500 text-center mb-6 animate-fadeIn">Login</h2>
+      relative before:absolute before:inset-0 before:border before:border-lime-800 before:rounded-xl before:shadow-[0_0_15px_#84cc16] before:animate-pulse mb-30">
+      <h2 className="text-2xl font-bold text-lime-600 text-center mb-6 animate-fadeIn">Login</h2>
       <form className="animate-fadeIn">
         <CampoLogin label="Usuário:" type="text" name="username" />
         <CampoLogin label="Senha:" type="password" name="password" />
